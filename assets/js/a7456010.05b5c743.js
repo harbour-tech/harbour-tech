824ef22c08ed368ac0ed49812428002202f95313
Martin <MartinBechtle@users.noreply.github.com>
2024-11-12 16:06:09 +0100

    first commit

---
+++ assets/js/a7456010.05b5c743.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapi_docs_generator=self.webpackChunkapi_docs_generator||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/aba21aa0.60d2a5c5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapi_docs_generator=self.webpackChunkapi_docs_generator||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
